6f065042c3e1c7be37487c499feb29f5940b3606
Gavin Kennedy <gavin@kennedy.co.za>
2020-05-04 16:28:38 +0200

    assembly and more-info pages

---
+++ assembly.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=3.0, minimum-scale=0.5, user-scalable=yes" />
+
+	<!--
+	PARTIAL > Google Tag Manager head script
+	-->
+	<?php include("assets/partials/google-tag-manager-head.html"); ?>
+
+	<!--
+	PARTIAL > Global scripts
+	-->
+	<?php include("assets/partials/global-scripts.html"); ?>
+
+	<!--
+	LINK & PARTIAL > Async-load non-critical CSS
+	-->
+
+	<link rel="preload" href="assets/non-critical.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+	<noscript><link rel="stylesheet" href="assets/non-critical.min.css"></noscript>
+	<?php include("assets/partials/loadCSS.html"); ?>
+
+	<meta name="robots" content="noindex, nofollow">
+	<meta name="pagename" content="Education Incorporated Boutique School">
+	<meta name="HandheldFriendly" content="true">
+	<meta name="MobileOptimized" content="320">
+
+	<!--
+	PARTIAL > Base URL
+	-->
+	<?php include("assets/partials/base-url.html"); ?>
+
+	<title>Assembly</title>
+	<link rel="icon" type="image/png" sizes="32x32" href="favicon.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="apple-touch-icon.png">
+	<link rel="dns-prefetch" href="https://educationincorporated.co.za/">
+
+	<!--
+	STYLES > Critical CSS
+	-->
+	<style type="text/css">
+		<?php include("assets/critical.min.css"); ?>
+	</style>
+
+</head>
+
+<body class="theme-dark-grey">
+
+	<!--
+	PARTIAL > Google Tag Manager body script
+	-->
+	<?php include("assets/partials/google-tag-manager-body.html"); ?>
+
+	<!--
+	PARTIAL > Browse Happy outdated browser notice
+	-->
+	<?php include("assets/partials/browse-happy.html"); ?>
+	
+	<!--
+	PARTIAL > Page leader with logo and desktop menu
+	-->
+	<?php include("assets/partials/page-leader.html"); ?>
+
+	<main>
+
+		<!--
+		Main content
+		-->
+		<section class="pad-respond-xy-mob theme-white border-b-mob shadow-t-mob">
+		
+			<h1 class="pad-respond-t-mob text-underline-red">Welcome to Assembly :-)</h1>
+
+			<p>Our next assembly Meet starts at 07h30 on Tuesday 5 May 2020 — please connect from 07h15</p>
+
+			<div class="flow-ttb-mob flow-ltr-tab constrain pad-respond-y-mob align-fs-tab">
+
+				<!--
+				Main content
+				-->
+				<p>Ensure you have the <a href="https://chrome.google.com/webstore/detail/google-meet-grid-view/kklailfgofogmmdlhgmjgenehkjoioip?hl=en">Chrome Grid View Extension</a> installed and enabled. Then you can <a href="https://tiny.cc/eduincassembly">click here </a> to check if assembly has started. Remember to <strong>MUTE</strong> your microphone and leave your <strong>CAMERA ON</strong>.</p>
+
+			</div>
+
+		</section>
+
+	</main>
+
+	<!--
+	PARTIAL > Page footer
+	-->
+	<?php include("assets/partials/page-footer.html"); ?>
+
+</body>
+
+</html>
---
+++ thank-you-more-info.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=3.0, minimum-scale=0.5, user-scalable=yes" />
+
+	<!--
+	PARTIAL > Google Tag Manager head script
+	-->
+	<?php include("assets/partials/google-tag-manager-head.html"); ?>
+
+	<!--
+	PARTIAL > Global scripts
+	-->
+	<?php include("assets/partials/global-scripts.html"); ?>
+
+	<!--
+	LINK & PARTIAL > Async-load non-critical CSS
+	-->
+
+	<link rel="preload" href="assets/non-critical.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+	<noscript><link rel="stylesheet" href="assets/non-critical.min.css"></noscript>
+	<?php include("assets/partials/loadCSS.html"); ?>
+
+	<meta name="robots" content="noindex, nofollow">
+	<meta name="pagename" content="Education Incorporated Boutique School">
+	<meta name="HandheldFriendly" content="true">
+	<meta name="MobileOptimized" content="320">
+
+	<!--
+	PARTIAL > Base URL
+	-->
+	<?php include("assets/partials/base-url.html"); ?>
+
+	<title>Thank You!</title>
+	<link rel="icon" type="image/png" sizes="32x32" href="favicon.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="apple-touch-icon.png">
+	<link rel="dns-prefetch" href="https://educationincorporated.co.za/">
+
+	<!--
+	STYLES > Critical CSS
+	-->
+	<style type="text/css">
+		<?php include("assets/critical.min.css"); ?>
+	</style>
+
+</head>
+
+<body class="theme-dark-grey">
+
+	<!--
+	PARTIAL > Google Tag Manager body script
+	-->
+	<?php include("assets/partials/google-tag-manager-body.html"); ?>
+
+	<!--
+	PARTIAL > Browse Happy outdated browser notice
+	-->
+	<?php include("assets/partials/browse-happy.html"); ?>
+	
+	<!--
+	PARTIAL > Page leader with logo and desktop menu
+	-->
+	<?php include("assets/partials/page-leader.html"); ?>
+
+	<main>
+
+		<!--
+		Main content
+		-->
+		<section class="pad-respond-xy-mob theme-white border-b-mob shadow-t-mob">
+		
+			<h1 class="pad-respond-t-mob text-underline-red">Thank you — We have received your request for additional information</h1>
+
+			<div class="flow-ttb-mob flow-ltr-tab constrain pad-respond-y-mob align-fs-tab">
+
+				<!--
+				Main content
+				-->
+				<p>A mail is on its way to you now, thank you for your interest in our school.</p>
+
+			</div>
+
+		</section>
+
+	</main>
+
+	<!--
+	PARTIAL > Page footer
+	-->
+	<?php include("assets/partials/page-footer.html"); ?>
+
+</body>
+
+</html>
