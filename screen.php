2f70acc5ad2d236c476b56d7ec8416308f6f3ddd
Gavin Kennedy <gavin@kennedy.co.za>
2021-01-30 13:01:41 +0200

    Create screen.php

---
+++ screen.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=3.0, minimum-scale=0.5, user-scalable=yes" />
+
+	<!--
+	PARTIAL > Google Tag Manager head script
+	-->
+	<?php include("assets/partials/google-tag-manager-head.html"); ?>
+
+	<!--
+	PARTIAL > Global scripts
+	-->
+	<?php include("assets/partials/global-scripts.html"); ?>
+
+	<!--
+	LINK & PARTIAL > Async-load non-critical CSS
+	-->
+
+	<link rel="preload" href="assets/non-critical.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+	<noscript><link rel="stylesheet" href="assets/non-critical.min.css"></noscript>
+	<?php include("assets/partials/loadCSS.html"); ?>
+
+	<meta name="robots" content="noindex, nofollow">
+	<meta name="pagename" content="Education Incorporated Boutique School">
+	<meta name="HandheldFriendly" content="true">
+	<meta name="MobileOptimized" content="320">
+
+	<!--
+	PARTIAL > Base URL
+	-->
+	<?php include("assets/partials/base-url.html"); ?>
+
+	<title>Covid-19 Screening</title>
+	<link rel="icon" type="image/png" sizes="32x32" href="favicon.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="apple-touch-icon.png">
+	<link rel="dns-prefetch" href="https://educationincorporated.co.za/">
+
+	<!--
+	STYLES > Critical CSS
+	-->
+	<style type="text/css">
+		<?php include("assets/critical.min.css"); ?>
+	</style>
+
+</head>
+
+<body class="theme-dark-grey">
+
+<meta http-equiv="refresh" content="2; url=https://form.jotform.com/201532927802553">
+
+
+	<!--
+	PARTIAL > Google Tag Manager body script
+	-->
+	<?php include("assets/partials/google-tag-manager-body.html"); ?>
+
+	<!--
+	PARTIAL > Browse Happy outdated browser notice
+	-->
+	<?php include("assets/partials/browse-happy.html"); ?>
+	
+	<!--
+	PARTIAL > Page leader with logo and desktop menu
+	-->
+	<?php include("assets/partials/page-leader.html"); ?>
+
+	<main>
+
+		<!--
+		Main content
+		-->
+		<section class="pad-respond-xy-mob theme-white border-b-mob shadow-t-mob">
+		
+			<h1 class="pad-respond-t-mob text-underline-red">Covid-19 Screening</h1>
+
+			<p>Keeping each other safe!</p>
+			<p><a href="https://pod.link/1439099991">Online Screening Form</a></p>
+			
+			<div class="flow-ttb-mob flow-ltr-tab constrain pad-respond-y-mob align-fs-tab">
+
+				<!--
+				Main content
+				-->
+			
+			</div>
+
+		</section>
+
+	</main>
+
+	<!--
+	PARTIAL > Page footer
+	-->
+	<?php include("assets/partials/page-footer.html"); ?>
+
+</body>
+
+</html>
