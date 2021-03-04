ca6a41da52e6c1a1e017526752acb9bf2f498c0e
Sheldon <info@lunia.co.za>
2021-03-04 10:05:32 +0400

    Add podcasts page

---
+++ podcasts.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=3.0, minimum-scale=0.5, user-scalable=yes" />
+
+	<!--
+	PARTIAL > Google Tag Manager head script
+	-->
+	<?php include("assets/partials/google-tag-manager-head.html"); ?>
+
+	<!--
+	PARTIAL > Global scripts
+	-->
+	<?php include("assets/partials/global-scripts.html"); ?>
+
+	<!--
+	LINK & PARTIAL > Async-load non-critical CSS
+	-->
+
+	<link rel="preload" href="assets/non-critical.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+	<noscript><link rel="stylesheet" href="assets/non-critical.min.css"></noscript>
+	<?php include("assets/partials/loadCSS.html"); ?>
+
+	<meta name="robots" content="index, follow">
+	<meta name="pagename" content="Education Incorporated Boutique School Podcasts">
+	<meta name="HandheldFriendly" content="true">
+	<meta name="MobileOptimized" content="320">
+
+	<!--
+	PARTIAL > Base URL
+	-->
+	<?php include("assets/partials/base-url.html"); ?>
+
+	<meta property="og:title" content="Education Incorporated Boutique School Podcasts" />
+	<meta property="og:keywords" content="" />
+	<meta property="og:description" content="" />
+	<meta property="og:locale" content="en_ZA" />
+	<meta property="og:url" content="https://educationincorporated.co.za/podcasts.php" />
+	<meta property="og:image" content="https://educationincorporated.co.za/assets/images/education-incorporated-logo.png" />
+	<meta property="og:type" content="website" />
+	<link rel="canonical" href="https://educationincorporated.co.za/podcasts.php" />
+	<title>Education Incorporated Boutique School Podcasts</title>
+	<link rel="icon" type="image/png" sizes="32x32" href="favicon.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="apple-touch-icon.png">
+	<link rel="dns-prefetch" href="https://educationincorporated.co.za/">
+	<!--
+	STYLES > Critical CSS
+	-->
+	<style type="text/css">
+		<?php include("assets/critical.min.css"); ?>
+	</style>
+
+</head>
+
+<body class="theme-dark-grey">
+
+	<!--
+	PARTIAL > Google Tag Manager body script
+	-->
+	<?php include("assets/partials/google-tag-manager-body.html"); ?>
+
+	<!--
+	PARTIAL > Browse Happy outdated browser notice
+	-->
+	<?php include("assets/partials/browse-happy.html"); ?>
+
+	<!--
+	PARTIAL > Page leader with logo and desktop menu
+	-->
+	<?php include("assets/partials/page-leader.html"); ?>
+
+    <!--
+    Page header
+    -->
+
+    <header class="flow-ttb-mob constrain pad-x-mob pad-double-y-mob">
+
+        <h1 class="text-underline-red">Our Podcast Channel</h1>
+        <div class="h3 pad-respond-t-mob">Our podcast channel will give you some insight into how EduInc operates and communicates with its community.</div>
+
+    </header>
+
+	<main>
+
+		<!--
+		Main content
+		-->
+		<section class="pad-respond-xy-mob theme-white border-b-mob shadow-t-mob">
+
+            <iframe src="https://iframe.iono.fm/c/6057?accent=37377f&description=1&sharing=1" width="100%" height="100%" frameborder="0"></iframe>
+
+		</section>
+
+		<!--
+		PARTIAL > Photo strip
+		-->
+		<?php include("assets/partials/photo-strip.html"); ?>
+
+		<!--
+		PARTIAL > Contact form
+		-->
+		<?php include("assets/partials/contact-form.html"); ?>
+
+	</main>
+
+	<!--
+	PARTIAL > Page footer
+	-->
+	<?php include("assets/partials/page-footer.html"); ?>
+
+</body>
+
+</html>
