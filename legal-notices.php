56531fab895a28d95ede7bf0b4ea9615d4c13752
Gavin Kennedy <gavin@kennedy.co.za>
2021-04-02 12:24:38 +0200

    legal notices

---
+++ legal-notices.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=3.0, minimum-scale=0.5, user-scalable=yes" />
+
+	<!--
+	PARTIAL > Google Tag Manager head script
+	-->
+	<?php include("assets/partials/google-tag-manager-head.html"); ?>
+
+	<!--
+	PARTIAL > Global scripts
+	-->
+	<?php include("assets/partials/global-scripts.html"); ?>
+
+	<!--
+	LINK & PARTIAL > Async-load non-critical CSS
+	-->
+
+	<link rel="preload" href="assets/non-critical.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+	<noscript><link rel="stylesheet" href="assets/non-critical.min.css"></noscript>
+	<?php include("assets/partials/loadCSS.html"); ?>
+
+	<meta name="robots" content="noindex, nofollow">
+	<meta name="pagename" content="Education Incorporated Boutique School">
+	<meta name="HandheldFriendly" content="true">
+	<meta name="MobileOptimized" content="320">
+
+	<!--
+	PARTIAL > Base URL
+	-->
+	<?php include("assets/partials/base-url.html"); ?>
+
+	<title>Education Incorporated Legal Notices</title>
+	<link rel="icon" type="image/png" sizes="32x32" href="favicon.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="apple-touch-icon.png">
+	<link rel="dns-prefetch" href="https://educationincorporated.co.za/">
+
+	<!--
+	STYLES > Critical CSS
+	-->
+	<style type="text/css">
+		<?php include("assets/critical.min.css"); ?>
+	</style>
+
+</head>
+
+<body class="theme-dark-grey">
+
+	<!--
+	PARTIAL > Google Tag Manager body script
+	-->
+	<?php include("assets/partials/google-tag-manager-body.html"); ?>
+
+	<!--
+	PARTIAL > Browse Happy outdated browser notice
+	-->
+	<?php include("assets/partials/browse-happy.html"); ?>
+	
+	<!--
+	PARTIAL > Page leader with logo and desktop menu
+	-->
+	<?php include("assets/partials/page-leader.html"); ?>
+
+	<main>
+
+		<!--
+		Main content
+		-->
+		<section class="pad-respond-xy-mob theme-white border-b-mob shadow-t-mob">
+		
+			<h1 class="pad-respond-t-mob text-underline-red">Legal Notices</h1>
+
+			<div class="flow-ttb-mob flow-ltr-tab constrain pad-respond-y-mob align-fs-tab">
+
+				<!--
+				Main content
+				-->
+				<p><a href="https://drive.google.com/drive/folders/1_Zkw26U-X3b9C94haCltXYD_8Di4W1aJ?usp=sharing">Summary of all Legal Notices</a></p>
+				<p>​<a href="https://docs.google.com/document/d/1wNTlapjpBGSdet7rO293pOYEAUm6bEa8mZkT7XVOHHk/edit?usp=sharing">E-mail Disclaimer</a></p>
+				<p><a href="https://docs.google.com/document/d/1V1g0iqGlPm93uY6h4iz35E4MCVTfA6DAHlorxVS16lc/edit?usp=sharing">Privacy Policy - how we process your personal information</a></p>
+				<p>We are exempt from having an Access to Information or PAIA Manual.</p>
+
+			</div>
+
+		</section>
+
+		<!-- <section class="pad-respond-xy-mob theme-white border-b-mob shadow-t-mob">
+		
+			<h1 class="pad-respond-t-mob text-underline-red">E-mail Disclaimer</h1>
+
+			<div class="flow-ttb-mob flow-ltr-tab constrain pad-respond-y-mob align-fs-tab"> -->
+
+				<!--
+				Main content
+				-->
+				<!-- <p>​
+This message contains confidential information and is intended only for the intended recipients. If you are not an intended recipient you should not disseminate, distribute or copy this e-mail. Please notify us immediately by e-mail if you have received this e-mail by mistake and delete this e-mail from your system. E-mail transmission cannot be guaranteed to be secure or error-free as information could be intercepted, corrupted, lost, destroyed, arrive late or incomplete, or contain viruses. Therefore we do not accept liability for any errors or omissions in the contents of this message, which arise as a result of e-mail transmission. If verification is required please request a hard-copy version.​​</p>
+
+			</div>
+
+		</section> -->
+
+	</main>
+
+	<!--
+	PARTIAL > Page footer
+	-->
+	<?php include("assets/partials/page-footer.html"); ?>
+
+</body>
+
+</html>
